6d27db518b4d709da910c39d96dd765bc8474605
AleksGlinskiy <36837936+AleksGlinskiy@users.noreply.github.com>
2021-10-12 23:20:20 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,112 +1,85 @@
 document.addEventListener("DOMContentLoaded", function(){
     const api_url = "https://my-json-server.typicode.com/AleksGlinskiy/serv-demo/posts";
+    const list = document.getElementById("list");
+    const title = document.getElementById("title");
+    const modal = document.getElementById("photo_modal");
+    let page = 0;
 
-    async function getCollections(wrap, page) {
+    async function getTitle(item, container) {
+        container.innerHTML = "Загрузка...";
+        container.classList.add("load");
         const response = await fetch(api_url);
         if (response.ok) { 
-            let data = await response.json(); 
-            wrap.innerHTML ="";
+            let data = await response.json();
+            container.classList.remove("load");
 
-            if (data.length-1 >= page) {
-                data[page].collections.forEach(imgs => {
-                    wrap.innerHTML += elementdivCard(imgs);
-                });
-            } else {
-                wrap.innerHTML = "<h3>Данных нет!</h3>";
-            }
-        }
-        else { 
+            if (item < 0) item = page = data.length-1;
+            if (item > data.length-1) item = page = 0;
+            container.innerHTML = data[item].title;
+            
+        } else { 
             alert("Ошибка HTTP: " + response.status); 
         }
     }
 
-    async function getTitle(el, page) {
+    async function getCollections(item, container) {
+        container.classList.add("load");
         const response = await fetch(api_url);
-        if (response.ok) {
+        if (response.ok) { 
             let data = await response.json();
-            
-            if (data.length-1 >= page) {
-                el.innerHTML = data[page].title;
-            } else {
-                el.innerHTML = "Страница недоступна";
-            }
-        }
-        else {
-            alert("Ошибка HTTP: " + response.status);
+            container.innerHTML ="";
+            container.classList.remove("load");
+
+            if (item < 0) item = page = data.length-1;
+            if (item > data.length-1) item = page = 0;
+            data[item].collections.forEach(idx => container.innerHTML += componentCard(idx));
+        } else { 
+            alert("Ошибка HTTP: " + response.status); 
         }
     }
 
-    function elementdivCard(data) {
+    function componentCard(data) {
         let divCard = document.createElement("div");
         divCard.className = "card";
         divCard.innerHTML = `<a href="${data.full}" class="card__link"><img src="${data.thumb}" alt="${data.id}" class="card__img"></a>`;
         return divCard.outerHTML;
     }
 
-    function extractGet(name) {
-        let result = window.location.search.match(new RegExp(name + '=([^&=]+)'));
-        return result ? result[1] : false;
+    function updatePage(item) {
+        getTitle(item, title);
+        getCollections(item, list);
     }
 
-    function addOrUpdateUrlParam(name, value) {
-        var href = window.location.href;
-        var regex = new RegExp("[&\\?]" + name + "=");
-        if (regex.test(href) ) {
-            regex = new RegExp("([&\\?])" + name + "=\\d+");
-            window.location.href = href.replace(regex, "$1" + name + "=" + value);
-        }
-        else {
-            if(href.indexOf("?") > -1)
-            window.location.href = href + "&" + name + "=" + value;
-            else
-            window.location.href = href + "?" + name + "=" + value;
-        }
+    document.getElementById("page-next").onclick = function(event) {
+        event.preventDefault();
+        page += 1;
+        updatePage(page);
     }
 
-    function updateDataPage(new_title, new_list, new_page) {
-        if (arguments.length == 1) {
-            getTitle(title, arguments[0]);
-            getCollections(list, arguments[0]);
-            addOrUpdateUrlParam("page", arguments[0]);
-        } else {
-            getTitle(title, new_title);
-            getCollections(list, new_list);
+    document.getElementById("page-prev").onclick = function(event) {
+        event.preventDefault();
+        page -= 1;
+        updatePage(page);
+    }
+
+    list.onclick = function(event) {
+        event.preventDefault();
+        let target = event.target;
+        if (target.className != 'card__img') return; 
+        openModal(target.parentNode.getAttribute("href"));
+    }
     
-            if (new_page != undefined) {
-                addOrUpdateUrlParam("page", new_page);
-            }
-        }
+    function openModal(image) {
+        modal.querySelector(".modal__content").innerHTML = `<img src="${image}1" class="img-full" alt="modal">`;
+        modal.classList.add("open");
     }
 
-    document.getElementById("page-next").onclick = function(e) {
-        e.preventDefault();
-
-        page = Number(page) + 1;
-        updateDataPage(page);
+    modal.onclick = function(event) {
+        event.preventDefault();
+        let target = event.target;
+        if (target.className === 'img-full') return; 
+        this.classList.remove("open");
     }
 
-    document.getElementById("page-prev").onclick = function(e) {
-        e.preventDefault();
-
-        if (page <= 0) {
-            getTitle(title, 0);
-            updateDataPage(0, 0, page);
-        } else {
-            page = Number(page) - 1;
-            updateDataPage(page);
-        }
-    }
-
-    const list = document.getElementById("list");
-    const title = document.getElementById("title");
-    let page = extractGet("page");
-    if (page >= 0) {
-        updateDataPage(page, page);
-    } else {
-        updateDataPage(0, 0);
-    }
-
-    if (page === false) {
-        updateDataPage(0);
-    }
+    updatePage(page);
 });
